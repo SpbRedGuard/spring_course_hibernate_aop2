d0a9ff440d71ed05774d113b0de90ca2cca91a60
KMStr <kstrigalev@gmail.com>
2022-02-05 19:53:08 +0300

    учебное приложение на архитектуре MVC, с исползованием LoС и Di, Hibernate framework + AOP

---
+++ src/main/java/com/strigalev/spring/mvc_hibernate_aop/dao/EmployeeDao.java
@@ -0,0 +1,16 @@
+package com.strigalev.spring.mvc_hibernate_aop.dao;
+
+import com.strigalev.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDao {
+
+    List<Employee> getAllEmployees();
+
+    void saveEmployee(Employee employee);
+
+    Employee getEmployee(int id);
+
+    void deleteEmployee(int id);
+}
